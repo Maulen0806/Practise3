3452e8474fc3f094b0466b537a3fc7c72f3b008d
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-09 22:04:30 +0600

    Update class_methods.py

--- classes/class_methods.py
+++ classes/class_methods.py
@@ -1,1 +1,12 @@
+class cls:
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+    def __str__(self):
+        return f"I'm {self.name}, I was born in {year}"
+name = input()
+year = input()
+c1 = cls(name, year)
+print(c1)
+
 
