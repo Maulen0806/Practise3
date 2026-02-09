9f18cce697c39c689e332c8aaf14c76092e3f3b0
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-09 15:02:53 +0600

    Update return_values.py

--- functions/return_values.py
+++ functions/return_values.py
@@ -1,1 +1,10 @@
-
+def func(num):
+    for i in (2,3,5):
+        while num % i == 0:
+            num //= i
+    return num == 1
+n = int(input())
+if func(n):
+    print("Valid")
+else:
+    print("Not valid")
