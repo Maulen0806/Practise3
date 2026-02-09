838674bc9a4c35db8441d1082101ac99b1cf9744
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-09 15:31:27 +0600

    Update kwargs.py

--- functions/kwargs.py
+++ functions/kwargs.py
@@ -1,1 +1,5 @@
-
+def kwargs_me(**dict):
+    for key, value in dict.items():
+        print(f"{key}: {value}")
+d = {"name": "Maulenali", "age": 19, "hobby": "Videogames, gym"}
+print(kwargs_me(**d))
