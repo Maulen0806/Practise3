5e9b701ffcf043945335489286d05d6a3ccf7fd4
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-09 20:18:19 +0600

    Update init_method.py

--- classes/init_method.py
+++ classes/init_method.py
@@ -1,1 +1,9 @@
-
+class cls:
+    def __init__(self, name, age, hobby):
+        self.name = name
+        self.age = age
+        self.hobby = hobby
+c1 = cls("Maulenali", 19, "Gym, Videogames")
+print(c1.name)
+print(c1.age)
+print(c1.hobby)
