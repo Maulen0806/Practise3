18b6f45451625f21ec0a21168d5a83b0d057186b
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-09 21:33:02 +0600

    Update class_variables.py

--- classes/class_variables.py
+++ classes/class_variables.py
@@ -1,1 +1,10 @@
-
+class cls:
+    animal = "Wild animal"
+    def __init__(self, name):
+        self.name = name
+name = input()
+c1 = cls(name)
+print(c1.name)
+print(c1.animal)
+del cls.animal
+print(c1.animal)
