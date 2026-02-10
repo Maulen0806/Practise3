da949628eff5b6691fce9e860f92c2aa54aaac38
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-10 11:36:26 +0600

    Update super_function.py

--- inheritance/super_function.py
+++ inheritance/super_function.py
@@ -1,1 +1,13 @@
-
+class cls1():
+    def __init__(self, surname, name):
+        self.surname = surname
+        self.name = name
+    def me(self):
+        print(f"I'm {surname} {name}")
+class cls2(cls1):
+    def __init__(self, surname, name):
+        super().__init__(surname, name)
+name = input()
+surname = input()
+c1 = cls2(surname, name)
+c1.me()
