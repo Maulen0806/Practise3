15422bebe3e6afcf81f7e410d28f0ed5451ae501
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-10 11:44:35 +0600

    Update multiple_inheritance.py

--- inheritance/multiple_inheritance.py
+++ inheritance/multiple_inheritance.py
@@ -1,1 +1,16 @@
-
+class cls1():
+    def __init__(self, surname, name):
+        self.surname = surname
+        self.name = name
+    def me(self):
+        print(f"I'm {surname} {name}")
+class cls2(cls1):
+    def __init__(self, surname, name):
+        super().__init__(surname, name)
+class cls3(cls2):
+    def __init__(self, surname, name):
+        super().__init__(surname, name)
+name = input()
+surname = input()
+c1 = cls3(surname, name)
+c1.me()
