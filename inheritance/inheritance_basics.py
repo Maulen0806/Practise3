8acd4c435c0af1fcf9350b1259f8ef43fdc808fd
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-10 11:31:39 +0600

    Update inheritance_basics.py

--- inheritance/inheritance_basics.py
+++ inheritance/inheritance_basics.py
@@ -1,1 +1,12 @@
-
+class cls1():
+    def __init__(self, surname, name):
+        self.surname = surname
+        self.name = name
+    def me(self):
+        print(f"I'm {self.surname} {self.name}")
+class cls2(cls1):
+    pass
+name = input()
+surname = input()
+c1 = cls2(surname, name)
+c1.me()
