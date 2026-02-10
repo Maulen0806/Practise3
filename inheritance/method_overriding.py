bcd9ed67671e7fb04625608c7f64569374d327b8
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-10 19:34:39 +0600

    Update method_overriding.py

--- inheritance/method_overriding.py
+++ inheritance/method_overriding.py
@@ -1,1 +1,18 @@
-
+class animal:
+    def __init__(self, name):
+        self.name = name
+class dog(animal):
+    def speak(self):
+        return "Woof-woof"
+class cat(animal):
+    def __init__(self, name, color):
+        super().__init__(name)  
+        self.color = color  
+    def speak(self):  
+        return "Meow"
+i1 = dog("Aktos")
+i2 = cat("Murkabai", "Grey")
+i3 = cat("Tigra", "Orange")
+print(i1.speak())
+print(i2.speak())
+print(i3.color)
